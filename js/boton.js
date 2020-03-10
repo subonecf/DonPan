23afc3641207676c1a77852afe02d9c71f82844c
subonecf <subonecf@gmail.com>
2020-03-10 19:28:50 -0400

    modificaciones al header

--- js/boton.js
+++ js/boton.js
@@ -8,4 +8,25 @@ $(document).ready(function () {
     $("#btn-toogle3").on("click", function () {
         $(".content4").slideToggle();
     });
+
+    $(function() {
+        function slideMenu() {
+          var activeState = $(".container-menu .menu-list").hasClass("active");
+          $(".container-menu .menu-list").animate({left: activeState ? "0%" : "-150%"}, 400);
+        }
+        $("#menu-wrapper").click(function(event) {
+          event.stopPropagation();
+          $("#hamburger-menu").toggleClass("open");
+          $(".container-menu .menu-list").toggleClass("active");
+          slideMenu();
+      
+          $("body").toggleClass("overflow-hidden");
+        });
+
+    });
+
+    
+
 });
+
+
