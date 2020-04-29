0ef3aeb66b48499d29c66d6084a8f89f76476811
subonecf <subonecf@gmail.com>
2020-04-29 15:28:15 -0400

    modificacion el menu

--- js/script.js
+++ js/script.js
@@ -35,6 +35,16 @@ $(document).ready(function () {
         }
     }
 
+    $(function() {
+        $(document).on('click', 'a.page-scroll', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top-71
+            }, 900, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
 
 });
 
